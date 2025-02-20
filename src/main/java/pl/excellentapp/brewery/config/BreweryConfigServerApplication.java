7833317ecd3ff3180947f501016d10064ec4c7be
Adrian Pionka <adrian@pionka.com>
2025-02-20 20:45:45 +0100

    init brewery config server

---
+++ src/main/java/pl/excellentapp/brewery/config/BreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/excellentapp/brewery/config/BreweryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
